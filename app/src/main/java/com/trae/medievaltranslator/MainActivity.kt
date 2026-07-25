7d49eb4b556693a5d4005e4a05ff527d212f1985
alijhhggg <alireza010122@gmail.com>
2026-07-25 15:01:57 +0330

    Update MainActivity.kt

--- app/src/main/java/com/trae/medievaltranslator/MainActivity.kt
+++ app/src/main/java/com/trae/medievaltranslator/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.trae.medievaltranslator
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +15,15 @@ class MainActivity : AppCompatActivity() {
 
         val btnStart = findViewById<Button>(R.id.btnStart)
         btnStart?.setOnClickListener {
-            Toast.makeText(this, "مترجم فعال شد!", Toast.LENGTH_SHORT).show()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+                val intent = Intent(
+                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:$packageName")
+                )
+                startActivity(intent)
+            } else {
+                startService(Intent(this, FloatingService::class.java))
+            }
         }
     }
 }
